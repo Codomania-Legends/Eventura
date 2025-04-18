43879683e065dec014414802d270cb7f8909845d
VidhiAgrawa <vidhiagrawal2205@gmail.com>
2025-04-19 01:55:27 +0530

    adding

---
+++ frontend/src/Components/PastEvent/Pastevent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Pastevent.css'
+function Pastevent() {
+  return (
+    <>
+        <main className="past-main flex">
+            <div className="div-box1">
+
+            </div>
+            <div className="div-box2"></div>
+            <div className="div-box3"></div>
+        </main>
+    </>
+  )
+}
+
+export default Pastevent
