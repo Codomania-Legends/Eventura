80bc9ff80b625088c93698e4e86e80926d4d3191
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-07-09 00:22:44 +0530

    initializing Frontend

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const express = require("express")
+const PORT = process.env.PORT
+const app = express()
+
+app.get( "/" , ( req , res ) => {
+    res.send("Helllo")
+} )
+
+app.listen( PORT , () => console.log(`Server Started at PORT : ${PORT}`) )
