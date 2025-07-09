2a8cfc5e274cdd562854fe9a21133bd285c117ca
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-07-09 13:47:24 +0530

    Commit1 9th July

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import Signup from './Components/LoginSignup/Signup'
 import Login from "./Components/LoginSignup/Login.jsx"
 import DashEvent from "./Components/DashBoard/DashEvent/DashEvent.jsx"
+import ManageEvent from "./Components/DashBoard/DashManageEvent/ManageEvent.jsx"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
       <Route path="/signup" element={<Signup/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/dashboard/events" element={<DashEvent />}></Route>
+      <Route path="/dashboard/m-event" element={<ManageEvent />}></Route>
     </Routes>
   </BrowserRouter>
 )
